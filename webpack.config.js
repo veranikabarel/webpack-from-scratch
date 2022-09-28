de52ae8fb9013469a0c58a2289e65e1eea14b405
Veranika Kasparevych <veranika.kasparevych@espeo.eu>
2022-09-28 14:11:21 +0200

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -8,13 +8,35 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "[name].js",
+    filename: "[name][contenthash].js",
+    clean: true,
+  },
+  devtool: "source-map",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
         use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babe/preset-env"],
+          },
+        },
       },
     ],
   },
