17e446e14e716fcbf7a4d545e672af3cef222c33
Veranika Kasparevych <veranika.kasparevych@espeo.eu>
2022-09-28 14:54:07 +0200

    add api

--- src/generateJoke.js
+++ src/generateJoke.js
@@ -1,5 +1,14 @@
+import axios from "axios";
+
 let generateJoke = () => {
-  return "I dont't trust stairs. They're always up to something";
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+  axios.get("https://icanhazdadjoke.com", config).then((res) => {
+    document.getElementById("joke").innerHTML = res.data.joke;
+  });
 };
 
 export default generateJoke;
--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "[name][contenthash].js",
     clean: true,
+    assetModuleFilename: "[name][ext]",
   },
   devtool: "source-map",
   devServer: {
@@ -34,9 +35,13 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babe/preset-env"],
+            presets: ["@babel/preset-env"],
           },
         },
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
       },
     ],
   },
